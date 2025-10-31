e9b82e59f36feece46aec7842fdf603fe54f7a9f
JonNKolb <Jonked@gmail.com>
2025-10-31 15:00:14 -0400

    started work on creating website

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
-from textnode import * 
+import shutil
+import os
 
 def main():
-    node = TextNode("dummy Text", TextType.LINK, "www.dummyurl.gov")
-    print(node)
+    delete_contents("./public")
+    copy_contents("./static","./public")
+    return
+
+def delete_contents(pth):
+    contents = os.listdir(pth)
+    for content in contents:
+        content_path = os.path.join(pth, content)
+        if os.path.isfile(content_path) == True:
+            os.remove(content_path)
+        else:
+            shutil.rmtree(content_path)
+
+def copy_contents(source, target):
+    contents = os.listdir(source)
+    for content in contents:
+        content_path = os.path.join(source, content)
+        if os.path.isfile(content_path) == True:
+            shutil.copy(content_path, target)
+        else:
+            target_dir = os.path.join(target, content)
+            os.mkdir(target_dir)
+            copy_contents(content_path, target_dir)
 
 main()
