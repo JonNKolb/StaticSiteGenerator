804a5142e59a375e9f1917c71ce3e1326ebf7f2f
JonNKolb <Jonked@gmail.com>
2025-11-02 00:30:02 -0400

    Active webpage on github

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import shutil
 import os
-import re
+import sys
 from Block_to_HTML import markdown_to_html_node
 from extract_markdown import extract_title
 
 def main():
-    delete_contents("./public")
-    copy_contents("./static","./public")
-    generate_page("./content","./template.html","./public")
+    basepath = sys.argv[0]
+    delete_contents("./docs")
+    copy_contents("./static","./docs")
+    generate_page("./content","./template.html","./docs", basepath)
 
 def delete_contents(pth):
     contents = os.listdir(pth)
@@ -41,7 +42,7 @@ def identify_pages(source):
             md_files.extend(identify_pages(content_path))
     return md_files
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     md_files = identify_pages(from_path)
     for file_path in md_files:
         file_dest = file_path.replace(from_path, dest_path).replace(".md",".html")
@@ -54,6 +55,8 @@ def generate_page(from_path, template_path, dest_path):
         header = extract_title(markdown)
         template = template.replace("{{ Title }}", header)
         template = template.replace("{{ Content }}", html.to_html())
+        template = template.replace('href="/', f'href="{basepath}')
+        template = template.replace('src="/', f'src="{basepath}')
         directory = os.path.dirname(file_dest)
         os.makedirs(directory, exist_ok=True)
         with open(file_dest, mode='x') as file:
